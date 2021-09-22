3c1716c20a90e4b928c72d4274b26d6fd77b0325
Laila-Mo <89812233+Laila-Mo@users.noreply.github.com>
2021-09-22 22:38:37 +0200

    Add files via upload

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,30 @@
+import Img from '../../images/Image-about-section.png';
+import Img2 from '../../images/contact2.jpg';
+export const homeObj1={
+    id:'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine:'Delight',
+    headLine:'We are bringing you a unique experience !',
+    description:
+        'Our methodology allow us to understand recipes at a deeper level, and recommend recipes to our users based on their diets, allergies, tastes, and more .',
+    imgStart:false,
+    img: Img,
+    alt:'food',
+    darkText: false
+};
+export const homeObj2={
+    id:'contact',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine:'Get in touch',
+    headLine:'For any inquiries or expert advice please contact',
+    description:
+        ' Email : Delight@email.com ' ,
+    imgStart:true,
+    img: Img2,
+    alt:'food',
+    darkText: true
+};
