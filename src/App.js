d56241d8f11b16d746c23a4242113e83a9fe9da8
Laila-Mo <89812233+Laila-Mo@users.noreply.github.com>
2021-09-22 22:32:53 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {BrowserRouter as Router,Switch,Route} from'react-router-dom';
+
+import GlobalStyle from "./globalStyles";
+import Home from "./pages/Home";
+import recipes from "./pages/recipes";
+import Login from "./pages/Login";
+import Details from "./pages/Details";
+
+function App() {
+  
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/recipes" component={recipes} exact/>
+        <Route path="/Login" component={Login} exact/>
+        <Route path="/:Details" component={Details} exact/>
+        
+      </Switch>
+      <GlobalStyle/>
+    </Router>
+  );
+}
+export default App;
